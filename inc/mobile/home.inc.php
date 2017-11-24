e4703229f576c1231bb1c94e7866905be8b1cc09
zxh5566 <1045746362@qq.com>
2017-11-24 11:12:26 +0800

    11/24-1

--- inc/mobile/home.inc.php
+++ inc/mobile/home.inc.php
@@ -3,9 +3,15 @@
 global $_W;
 include 'function.php';
 
-$user=pdo_fetchall("SELECT * FROM".tablename('hulu_love_user')."WHERE uniacid=:uniacid ORDER BY uid ASC",array(':uniacid'=>$_W['uniacid']));
-
-
+$vipuser=pdo_fetchall("SELECT * FROM".tablename('hulu_love_user')."WHERE uniacid=:uniacid and upid=4 ORDER BY uid desc limit 12",array(':uniacid'=>$_W['uniacid']));
+foreach($vipuser as $k=>$v){
+	$vipuser[$k]['photo'] = pdo_fetch("select * from ".tablename('hulu_love_photos')." where uniacid={$_W['uniacid']} and openid = '{$v['openid']}' order by pic_time desc limit 1 ");
+}
+//print_r($user);exit;
+$guanli = pdo_fetch('SELECT * FROM' . tablename('hulu_love_guanli') . 'WHERE uniacid=:uniacid', array(':uniacid' => $_W['uniacid']));
+$ads = pdo_fetchall('SELECT * FROM' . tablename('hulu_love_ads') . 'WHERE uniacid=:uniacid AND ads_pid=:ads_pid ORDER BY ads_did ASC', array(':uniacid' => $_W['uniacid'], ':ads_pid' => '1'));
+$moreads = pdo_fetch('SELECT * FROM' . tablename('hulu_love_moreads') . " where uniacid={$_W['uniacid']} and  moreads_id = 1");
+$signup = pdo_fetch('SELECT * FROM' . tablename('hulu_love_active') . " where uniacid={$_W['uniacid']} and  active_type = 2 and active_pid=4 order by active_time desc limit 1");
 include $this->template('home');
 
 ?>
