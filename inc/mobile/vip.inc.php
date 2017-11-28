6b17330521f050cb7a09a7fa6657f4bfc209de02
nxp913 <nxp913@sina.com>
2017-11-28 16:29:04 +0800

    nxp

--- inc/mobile/vip.inc.php
+++ inc/mobile/vip.inc.php
@@ -3,6 +3,12 @@
 global $_W,$_GPC;
 include 'function.php';
 
+$user = pdo_fetch("SELECT * FROM".tablename('hulu_love_user')."WHERE uniacid=:uniacid and openid=:openid",array(':uniacid'=>$_W['uniacid'],':openid'=>$_W['openid']));
+if($user['ureal'] != '2'){
+	//已实名认证
+	message('实名认证后才可升级VIP会员！',$this->createMobileUrl('real'),'error');die;
+}
+
 $make=pdo_fetch("SELECT * FROM".tablename('hulu_love_make')."WHERE uniacid=:uniacid",array(':uniacid'=>$_W['uniacid']));
 
 $order_num=date('YmdHis').substr(implode(NULL, array_map('ord', str_split(substr(uniqid(), 7, 13), 1))), 0, 8);
