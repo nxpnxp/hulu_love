9795b03dccac5077b13e4f2962364426753512d8
nxp913 <nxp913@sina.com>
2017-12-18 10:48:39 +0800

    nxp

--- inc/mobile/active_view.inc.php
+++ inc/mobile/active_view.inc.php
@@ -25,5 +25,7 @@ $signup[$k]['active_starttime'] = date('Y-m-d H:i:s',$v['active_starttime']);
 $signup[$k]['active_endtime'] = date('Y-m-d H:i:s',$v['active_endtime']);	
 }
 
+$user=pdo_fetch("SELECT * FROM".tablename('hulu_love_user')."WHERE uniacid=:uniacid AND openid=:openid",array(':uniacid'=>$_W['uniacid'],':openid'=>$_W['openid']));
+
 include $this->template('active_view');
 ?>
--- inc/mobile/active_xhb.inc.php
+++ inc/mobile/active_xhb.inc.php
@@ -6,5 +6,11 @@ include 'function.php';
 $id = $_GPC['id'];
 $join=pdo_fetchall("SELECT * FROM".tablename('hulu_love_join')."WHERE uniacid=:uniacid AND active_id=:active_id AND join_pid=:join_pid",array(':uniacid'=>$_W['uniacid'],':active_id'=>$id,':join_pid'=>'3'));
 
+$boy=pdo_fetchcolumn("SELECT count(*) FROM".tablename('hulu_love_join')."WHERE uniacid=:uniacid AND active_id=:active_id AND join_pid=:join_pid and join_sex=1",array(':uniacid'=>$_W['uniacid'],':active_id'=>$id,':join_pid'=>'3'));
+$girl=pdo_fetchcolumn("SELECT count(*) FROM".tablename('hulu_love_join')."WHERE uniacid=:uniacid AND active_id=:active_id AND join_pid=:join_pid and join_sex=2",array(':uniacid'=>$_W['uniacid'],':active_id'=>$id,':join_pid'=>'3'));
+
+$boy = empty($boy) ? 0 : $boy;
+$girl = empty($girl) ? 0 : $girl;
+
 include $this->template('active_xhb');
 ?>
